08ebf32a8a3308f2daa032e67db20ae4e2d61467
Sebastian Desaulniers <seaofthebass@gmail.com>
2019-03-08 14:35:41 -0500

    localstorage

--- reconstruct.js
+++ reconstruct.js
@@ -1,15 +1,15 @@
 function generateTinyUrl(link) {
-    console.log(link);
     $.get(`https://cors.io/?http://tinyurl.com/api-create.php?url=${link}`, function (newUrl) {
         $("#result").val(newUrl)
-        $("#result").select()
-        // document.execCommand('copy')
-        window.open(newUrl,'_blank')
+        localStorage.setItem('link', document.getElementById("result").value);
+
+        window.open(document.getElementById('result').value, '_blank')
+
     });
 }
 
 function generateLink(puzzle, scramble, moves) {
-    let link = `https://alg.cubing.net/?puzzle=${puzzle}&setup=${scramble}&alg=${moves}&type=reconstruction&view=playback` 
+    let link = `https://alg.cubing.net/?puzzle=${puzzle}&setup=${scramble}&alg=${moves}&type=reconstruction&view=playback`
     generateTinyUrl(link);
 }
 
@@ -19,4 +19,15 @@ function generate() {
     var moves = $("#moves").val().replace(/ /g, '_').replace(/'/g, '-');
 
     generateLink(puzzle, scramble, moves);
+    localStorage.setItem('puzzle', document.getElementById("puzzle").value);
+    localStorage.setItem('scramble', document.getElementById("scramble").value);
+    localStorage.setItem('moves', document.getElementById("moves").value);
+}
+
+window.onload = function () {
+    $("#puzzle").val(localStorage.getItem('puzzle'))
+    $("#scramble").val(localStorage.getItem('scramble'))
+    $("#moves").val(localStorage.getItem('moves'))
+    $("#result").val(localStorage.getItem('link'))
+
 }
