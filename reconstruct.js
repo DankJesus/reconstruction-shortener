2683c547a68edd1e8c4b97ddb05ba6726f40da58
Sebastian Desaulniers <seaofthebass@gmail.com>
2019-03-08 14:13:23 -0500

    nvm, i just suck at coding. everything should work now

--- reconstruct.js
+++ reconstruct.js
@@ -1,21 +1,21 @@
 function generateTinyUrl(link) {
+    console.log(link);
     $.get(`https://cors.io/?http://tinyurl.com/api-create.php?url=${link}`, function (newUrl) {
-        return newUrl;
+        $("#result").val(newUrl)
+        $("#result").select()
+        document.execCommand('copy')
     });
 }
 
 function generateLink(puzzle, scramble, moves) {
-    let link = `alg.cubing.net/?puzzle=${puzzle}&setup=${scramble}&alg=${moves}&type=reconstruction&view=playback` 
-    let result = generateTinyUrl(link);
-    return result;
+    let link = `https://alg.cubing.net/?puzzle=${puzzle}&setup=${scramble}&alg=${moves}&type=reconstruction&view=playback` 
+    generateTinyUrl(link);
 }
 
 function generate() {
-    var puzzle = $("#puzzle").find(":selected").value();
-    var scramble = $("#scramble").html().replace(/ /g, '_').replace(/'/g, '-');
-    var moves = $("#moves").html().replace(/ /g, '_').replace(/'/g, '-');
+    var puzzle = $("#puzzle").find(":selected").val();
+    var scramble = $("#scramble").val().replace(/ /g, '_').replace(/'/g, '-');
+    var moves = $("#moves").val().replace(/ /g, '_').replace(/'/g, '-');
 
-    let res = generateLink(puzzle, scramble, moves);
-    console.log(res);
-    $("#result").html(res)
+    generateLink(puzzle, scramble, moves);
 }
