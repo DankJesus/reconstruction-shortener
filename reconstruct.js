5a3bc8990c1455f75b511c7f87d81418e2b6794e
Sebastian Desaulniers <seaofthebass@gmail.com>
2019-03-08 13:56:43 -0500

    i seriously suck at webdev

--- reconstruct.js
+++ reconstruct.js
@@ -1,19 +1,21 @@
 function generateTinyUrl(link) {
-    $.get(`https://tinyurl.com/api-create.php?url=${link}`, function (newUrl) {
+    $.get(`http://tinyurl.com/api-create.php?url=${link}`, function (newUrl) {
         return newUrl;
     });
 }
 
 function generateLink(puzzle, scramble, moves) {
-    let link = `https://alg.cubing.net/?puzzle=${puzzle}&setup=${scramble}&alg=${moves}&type=reconstruction&view=playback` 
+    let link = `https://cors.io/?alg.cubing.net/?puzzle=${puzzle}&setup=${scramble}&alg=${moves}&type=reconstruction&view=playback` 
     let result = generateTinyUrl(link);
     return result;
 }
 
 function generate() {
-    var puzzle = $("#puzzle").value;
+    var puzzle = $("#puzzle").find(":selected").text();
     var scramble = $("#scramble").html().replace(/ /g, '_').replace(/'/g, '-');
     var moves = $("#moves").html().replace(/ /g, '_').replace(/'/g, '-');
 
-    $("#result").value = generateLink(puzzle, scramble, moves);
+    let res = generateLink(puzzle, scramble, moves);
+    console.log(res);
+    $("#result").html(res)
 }
